8e3f2dbf2dd638a80724b244ea77cc9321cc6882
Fernando Tsuda <ftsuda.senac@gmail.com>
2017-08-29 22:03:37 -0300

    Ajustes no pom.xml e classe principal

--- tadsstore-springmvc/src/main/java/br/senac/tads4/dswa/tadsstore/springmvc/TadsstoreSpringmvcApplication.java
+++ tadsstore-springmvc/src/main/java/br/senac/tads4/dswa/tadsstore/springmvc/TadsstoreSpringmvcApplication.java
@@ -2,11 +2,18 @@ package br.senac.tads4.dswa.tadsstore.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TadsstoreSpringmvcApplication {
+public class TadsstoreSpringmvcApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TadsstoreSpringmvcApplication.class, args);
-	}
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(TadsstoreSpringmvcApplication.class);
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(TadsstoreSpringmvcApplication.class, args);
+  }
 }
